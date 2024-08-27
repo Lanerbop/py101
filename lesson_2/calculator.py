ad7f6c8bbe37990ef66e5ffb1699a70a6aadb5f0
Lane <lanedwofford@gmail.com>
2024-08-27 01:53:43 +0000

    added pseudocode file

---
+++ lesson_2/calculator.py
@@ -0,0 +1,27 @@
+# ask user for the first number
+# ask user for the second number
+# ask user what operation to perform
+# perform the operation 
+# print to terminal
+
+print("Welcome to Calculator!")
+
+print("What's the first number?")
+number1 = input()
+print("What's the second number?")
+number2 = input()
+
+print("What operation would you like to perform?\n"
+      "1) Add 2) Subtract 3) Multiply 4) Divide")
+operation = input()
+
+if operation == "1":
+    output = int(number1) + int(number2)
+elif operation == "2":
+    output = int(number1) - int(number2)
+elif operation == "3":
+    output = int(number1) * int(number2)
+elif operation == "4":
+    output = int(number1) / int(number2)
+
+print(f"The result is: {output}")
---
+++ lesson_2/pseudocode.py
@@ -0,0 +1,45 @@
+# A FUNCTION THAT RETURNS THE SUM OF TWO NUMBERS
+
+# START
+
+# GET num1 and num2
+# PRINT num1 + num2
+
+def sum_(num1, num2):
+    return num1 + num2
+
+# FUNCTION TAKES LIST OF STRINGS AND RETURNS CONCATENATION
+
+# START
+
+# GET list of strings
+# PRINT join strings on ""
+
+def concatenate_strings(lst):
+    return "".join(lst)
+
+# print(concatenate_strings(["Hello", "World", "!"]))
+
+# START
+
+# GET list of integers
+# SET array = []
+# iterate through list
+# for index % 2 == 0, add integer to array
+# PRINT array
+
+# def every_other(lst):
+#     result = []
+#     index = 0
+#     while index < len(lst):
+#         if index % 2 == 0:
+#             result.append(lst[index])
+        
+#         index += 1
+
+#     return result
+
+# print(every_other([1, 2, 3, 4, 5, 6]))
+
+# START
+
