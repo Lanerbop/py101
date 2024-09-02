55e4d009a12a14ea6114e662ab0db3ab6f32a8ec
Lane <lanedwofford@gmail.com>
2024-09-02 18:01:33 +0000

    fixed variables that do not need to be global.

--- lesson_2/project_directory/payment_calculator.py
+++ lesson_2/project_directory/payment_calculator.py
@@ -1,106 +1,116 @@
 import os
 
-def clear():
-    os.system('clear')
+def calculator():
 
-def prompt(message):
-    return print(f"==> {message}")
+    def clear():
+        os.system('clear')
 
-def invalid_float(number_as_string):
-    if number_as_string in ["nan", "inf", "infinity"]:
-        return True
+    def prompt(message):
+        return print(f"==> {message}")
 
-    try:
-        float(number_as_string)
-    except ValueError:
-        return True
+    def invalid_float(number_as_string):
+        if number_as_string in ["nan", "inf", "infinity"]:
+            return True
 
-    if float(number_as_string) < 0:
-        return True
+        try:
+            float(number_as_string)
+        except ValueError:
+            return True
 
-    return False
+        if float(number_as_string) < 0:
+            return True
 
-def get_loan_outstanding():
-    prompt("What is the outstanding loan amount in dollars? (Ex: '13500')")
-    loan_outstanding = input()
+        return False
 
-    while invalid_float(loan_outstanding):
-        prompt("Please only type positive numbers. (Hint: Don't use '$'"
-            " or ',' in your number.)")
+    def get_loan_outstanding():
+        prompt("What is the outstanding loan amount in dollars? (Ex: '13500')")
         loan_outstanding = input()
-    
-    return loan_outstanding
 
-def get_apr():
-    prompt("What is the APR of the loan in percentage format? (Ex: '15.7')")
-    apr = input()
-    while invalid_float(apr):
-        prompt("Please only type positive numbers. (Hint: Don't use"
-               " the '%' sign.)")
+        while invalid_float(loan_outstanding):
+            prompt("Please only type positive numbers. (Hint: Don't use '$'"
+                " or ',' in your number.)")
+            loan_outstanding = input()
+
+        return loan_outstanding
+
+    def get_apr():
+        prompt("What is the APR of the loan in percentage format?"
+               " (Ex: '15.7')")
         apr = input()
-    
-    return apr
+        while invalid_float(apr):
+            prompt("Please only type positive numbers. (Hint: Don't use"
+                " the '%' sign.)")
+            apr = input()
 
-def display_welcome_message():
-    prompt("Welcome to Monthly Payment Calculator")
+        return apr
 
-def display_result():
-    prompt("Your monthly payment is: $" + str(monthly_payment))
+    def display_welcome_message():
+        prompt("Welcome to Monthly Payment Calculator")
 
-def display_try_again_question():
-    prompt("Would you like to use Monthly Payment Calculator again? y/n")
+    def display_result():
+        prompt("Your monthly payment is: $" + monthly_payment)
 
+    def display_try_again_question():
+        prompt("Would you like to use Monthly Payment Calculator again? y/n")
 
-clear()
-
-display_welcome_message()
-
-while True:
-    loan_outstanding = get_loan_outstanding()
-    loan_outstanding = float(loan_outstanding)
-
-    apr = get_apr()
-    apr = float(apr) / 100
-    monthly_apr = apr / 12
-
-
-    prompt("What is the outstanding loan duration in months?")
-    loan_duration_months = input()
-    while invalid_float(loan_duration_months):
-        prompt("Please only type numbers")
+    def get_loan_duration():
+        prompt("What is the outstanding loan duration in months?")
         loan_duration_months = input()
+        while invalid_float(loan_duration_months):
+            prompt("Please only type numbers")
+            loan_duration_months = input()
 
-    loan_duration_months = int(float(loan_duration_months))
-    # loan_duration_months rounds down to nearest integer if it
-    # is a decimal, which is desired and logical!
+        return loan_duration_months
 
-
-    # Monthly Payment Calculation
-    if loan_duration_months == 0:
-        monthly_payment = loan_outstanding
-    elif monthly_apr == 0:
-        monthly_payment = round((loan_outstanding / loan_duration_months), 2)
-    else:
-        monthly_payment = round((loan_outstanding * (monthly_apr / (1
-            - (1 + monthly_apr)**(-1 * loan_duration_months)))), 2)
-
-    monthly_payment = f"{monthly_payment:.2f}" # display to 2 decimal places
-
-
-    display_result()
-
-    display_try_again_question()
-    retry = input().lower()
-    while True:
-        if retry in ["y", "n"]:
-            break
-
-        prompt('Please enter "y" or "n".')
-        retry = input().lower()
-
-    if retry == 'n':
-        clear()
-        prompt("Goodbye!")
-        break
 
     clear()
+
+    display_welcome_message()
+
+
+    while True:
+        loan_outstanding = get_loan_outstanding()
+        loan_outstanding = float(loan_outstanding)
+
+        apr = get_apr()
+        apr = float(apr) / 100
+        monthly_apr = apr / 12
+
+        loan_duration_months = get_loan_duration()
+        loan_duration_months = int(float(loan_duration_months))
+        # loan_duration_months rounds down to nearest integer if input
+        # contains a decimal, which is desired and logical!
+
+
+        # Monthly Payment Calculation
+        if loan_duration_months == 0:
+            monthly_payment = loan_outstanding
+        elif monthly_apr == 0:
+            monthly_payment = round((loan_outstanding
+                                    / loan_duration_months), 2)
+        else:
+            monthly_payment = round((loan_outstanding * (monthly_apr /
+            (1- (1 + monthly_apr)**(-1 * loan_duration_months)))), 2)
+
+        monthly_payment = f"{monthly_payment:.2f}" # display to 2 decimal place
+
+
+        display_result()
+
+        display_try_again_question()
+        retry = input().lower()
+        while True:
+            if retry in ["y", "n"]:
+                break
+
+            prompt('Please enter "y" or "n".')
+            retry = input().lower()
+
+        if retry == 'n':
+            clear()
+            prompt("Goodbye!")
+            break
+
+        clear()
+
+calculator()
