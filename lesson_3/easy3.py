9de46e26adbc7def0d67787cfffa77c72e5570d0
Lane <lanedwofford@gmail.com>
2024-09-04 23:26:07 +0000

    finished easy3.py

---
+++ lesson_3/easy3.py
@@ -0,0 +1,30 @@
+# QUESTION 1
+
+# numbers = [1, 2, 3, 4]
+# numbers.clear()
+# while numbers:
+#     numbers.pop()
+# print(numbers)
+
+# QUESTION 2 -> wrong
+
+# the code will concatenate the lists
+
+# QUESTION 3 -> wrong
+
+# "hello there" will be printed because strings are immutable
+# the reassignment changes the pointer of str2, not the object
+# that it was pointing at
+
+# QUESTION 4
+
+# my list will be printed with the first value being 42 because
+# a shallow copy was made
+
+# QUESTION 5 -> didn't get second answer right
+
+# def is_color_valid(color):
+#     return bool(color == "blue" or color == "green")
+
+# def is_color_valid(color):
+#     return color in ['blue', 'green']
