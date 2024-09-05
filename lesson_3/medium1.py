48210531c32c449236b0df67170f102ffef9e69d
Lane <lanedwofford@gmail.com>
2024-09-05 16:42:48 +0000

    finished medium1

---
+++ lesson_3/medium1.py
@@ -0,0 +1,24 @@
+# QUESTION 1
+
+# flintstones_string = "The Flintstones Rock!"
+
+# for index in range(1, 11):
+#     print("-" * index + flintstones_string)
+
+# QUESTION 2
+
+# def factors(number):
+#     divisor = number
+#     result = []
+#     while divisor > 0:
+#         if number % divisor == 0:
+#             result.append(number // divisor)
+#         divisor -= 1
+#     return result
+
+# print(factors(10))
+# print(factors(-10))
+
+# QUESTION 3
+
+# the first one mutates the lists while the second reassigns buffer
